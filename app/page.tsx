d9498165633af2902c963293a64865c8c8bec16d
Apostolos Karamanis <apostkaram@gmail.com>
2025-09-20 19:11:36 +0300

    temprarily removed electric border

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,21 @@
 import Footer from "@/components/landing/footer/footer";
 import HeroSection from "@/components/landing/hero/hero";
 import TestimonialsSection from "@/components/landing/testimonial/testimonial";
-import ElectricBorder from "@/components/react-bits/electric-border";
+// import ElectricBorder from "@/components/react-bits/electric-border";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="relative">
       <div className="fixed top-[1rem] left-[1rem] z-101">
-        <ElectricBorder
+        <Image
+          src="/flow-lounge.svg"
+          alt="Logo"
+          width={80}
+          height={80}
+          className="rounded-full"
+        />
+        {/*<ElectricBorder
           color="#EF5021"
           speed={1}
           chaos={1}
@@ -22,7 +29,7 @@ export default function Home() {
             height={80}
             className="rounded-full"
           />
-        </ElectricBorder>
+        </ElectricBorder>*/}
       </div>
 
       <HeroSection />
