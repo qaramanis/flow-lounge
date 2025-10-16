abb2bfbc9d98967fda8a6f9320ecf218b42a8b6e
qaramanis <127772424+qaramanis@users.noreply.github.com>
2025-10-16 10:42:27 +0300

    Update membership.tsx

--- components/landing/membership/membership.tsx
+++ components/landing/membership/membership.tsx
@@ -56,7 +56,7 @@ export default function MembershipSection() {
                   <ActionButton
                     text="Become a member"
                     icon={ArrowUpRight}
-                    href="/contact"
+                    href="/community"
                   />
                 </div>
               </div>
