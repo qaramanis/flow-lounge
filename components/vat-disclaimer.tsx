66f057059811472d3c68b38f85a207d88ad13b82
qaramanis <127772424+qaramanis@users.noreply.github.com>
2025-11-07 22:07:19 +0200

    added vat-disclaimer

---
+++ components/vat-disclaimer.tsx
@@ -0,0 +1,11 @@
+export default function VatDisclaimer() {
+  return (
+    <div className="mt-16 text-center">
+      <p className="text-white/60 mb-3">
+        All prices listed above include vat 24% and municipal taxes 0.5%
+        <br />
+        Market Law Health Officer: Kyriakos Katikaridis
+      </p>
+    </div>
+  );
+}
